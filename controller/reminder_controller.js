9cbd1008134badf6d880a99afe6610dc98cf328e
ricadv <71803680+ricadv@users.noreply.github.com>
2020-11-04 11:35:12 -0800

    Add files via upload

---
+++ controller/reminder_controller.js
@@ -0,0 +1,57 @@
+let database = require("../database");
+
+let remindersController = {
+  // Show a list of reminders
+  list: (req, res) => {
+    res.render('reminder/index', { reminders: database.cindy.reminders })
+  },
+
+  // Show a Create Reminder Page
+  new: (req, res) => {
+    res.render('reminder/create')
+  },
+
+  // Show the details of a Single Reminder
+  listOne: (req, res) => {
+    let reminderToFind = req.params.id;
+    let searchResult = database.cindy.reminders.find(function (reminder) {
+      return reminder.id == reminderToFind;
+    })
+    if (searchResult != undefined) {
+      res.render('reminder/single-reminder', { reminderItem: searchResult })
+    } else {
+      res.render('reminder/index', { reminders: database.cindy.reminders })
+    }
+  },
+
+  // Create a reminder
+  // ⚠️ TODO: Currently hardcoded to always create a reminder for Cindy only. You need to make this dynamic. 
+  create: (req, res) => {
+    let reminder = {
+      id: database.cindy.reminders.length + 1,
+      title: req.body.title,
+      description: req.body.description,
+      completed: false
+    }
+    database.cindy.reminders.push(reminder);
+    res.redirect('/reminders');
+  },
+
+  // Show the Edit Reminder Page
+  edit: (req, res) => {
+    // ⭐️ your implementation here ⭐️
+
+  },
+
+  // Edit the Reminder
+  update: (req, res) => {
+    // ⭐️ your implementation here ⭐️
+  },
+
+  // Delete the Reminder
+  delete: (req, res) => {
+    // ⭐️ your implementation here ⭐️
+  }
+}
+
+module.exports = remindersController;
