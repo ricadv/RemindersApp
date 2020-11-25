b9a8179ee1cc30a267529fd0ee34cc2ad9982f7d
A01203501 <kbowles132@gmail.com>
2020-11-25 09:24:17 -0800

    fixed index code, adjusted placeholders

--- database.js
+++ database.js
@@ -1,9 +1,13 @@
 let Database = {
     cindy: {
-        reminders: [{id: 1, title: "abc", description: "abcabc", completed: false}]
+        reminders: [{id: 1, title: "abc", description: "abcabc", completed: false}],
+        friends: ["alex"]
     },
     alex: {
-        reminders: []
+        reminders: [{id: 2, title: "Social Reminder",
+        description: "Test the social reminder function",
+        completed: false}],
+        friends: ["cindy"]
     } 
 }
 
