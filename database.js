5ad21f916038401dd46364ecf9b71ec1785e5703
jsw13 <joseph.sze.wong@gmail.com>
2020-11-25 22:16:44 -0800

    changes to test database

--- database.js
+++ database.js
@@ -2,12 +2,16 @@ let Database = {
     "cindy@cindy.com": {
         email: "cindy@cindy.com",
         password: "cindy",
-        reminders: [{id: 1, title: "Cindy 1", description: "hello cindy", completed: false}]
+        reminders: [{id: 1, title: "Cindy 1", description: "hello cindy", completed: false}],
+        subtasks: [],
+        tags: []
     },
     "bobby@bobby.com": {
         email: "bobby@bobby.com",
         password: "bobby",
-        reminders: [{id: 1, title: "Bobby1", description: "hello bobby", completed: false}]
+        reminders: [{id: 1, title: "Bobby1", description: "hello bobby", completed: false}],
+        subtasks: [],
+        tags: []
     } 
 }
 
