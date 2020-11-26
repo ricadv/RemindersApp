3d24d61a202e29c7fd3e28c5b946e1c68486a157
jsw13 <joseph.sze.wong@gmail.com>
2020-11-25 22:24:15 -0800

    fixed data

--- database.js
+++ database.js
@@ -2,16 +2,13 @@ let Database = {
     "cindy@cindy.com": {
         email: "cindy@cindy.com",
         password: "cindy",
-        reminders: [{id: 1, title: "Cindy 1", description: "hello cindy", completed: false}],
-        subtasks: [],
-        tags: []
+        reminders: [{id: 1, title: "Cindy 1", description: "hello cindy", completed: false, subtasks: [{description: "subtask 1", completed: true}], tags: ["Greeting", "Hello"]}],
     },
     "bobby@bobby.com": {
         email: "bobby@bobby.com",
         password: "bobby",
-        reminders: [{id: 1, title: "Bobby1", description: "hello bobby", completed: false}],
-        subtasks: [],
-        tags: []
+        reminders: [{id: 1, title: "Bobby1", description: "hello bobby", completed: false, subtasks: [], tags: [] }],
+
     } 
 }
 
