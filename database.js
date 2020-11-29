cd1896cfda60123ba52970dd3348d9cf3482ee43
jsw13 <joseph.sze.wong@gmail.com>
2020-11-28 17:45:54 -0800

    username

--- database.js
+++ database.js
@@ -1,13 +1,13 @@
 let Database = {
     "cindy@cindy.com": {
-        username: "",
+        username: "cindy",
         email: "cindy@cindy.com",
         password: "cindy",
         reminders: [{id: 1, title: "Cindy 1", description: "hello cindy", completed: false, subtasks: [{description: "subtask 1", completed: false}], tags: ["Greeting", "Hello"]}],
         friends: ["bobby@bobby.com"]
     },
     "bobby@bobby.com": {
-        username: "",
+        username: "bobby",
         email: "bobby@bobby.com",
         password: "bobby",
         reminders: [{id: 1, title: "Bobby1", description: "hello bobby", completed: false, subtasks: [], tags: [] }],
